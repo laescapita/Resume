a1af0d052b217ae7eed161fdcf9d7dcbd2fd3a71
Leonel <leo.note0117@gmail.com>
2019-11-25 21:32:47 -0700

    Added Pic and IG

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GoalsComponent } from '../goals/goals.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalsComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +16,7 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+ 
+}
